ccbdbb5b7e4ca7e85077471e18a3f5ea6f18d8d5
jack-meyer-garvey <jackmeyergarvey@gmail.com>
2023-08-23 10:52:21 -0500

    Continuous collision with symbolic arithmetic. Added interact, backgrounds, and relative friction.

--- continuousCollision.py
+++ continuousCollision.py
@@ -185,6 +185,7 @@ def unfreezePhysics():
 
 def centerScreen():
     if Character.controlled is not None:
+        previous = Character.canvas.xview()[0]
         # Sets the position of the screen and text to follow the Character being controlled
         setScreenPositionX(float(Character.controlled.x) - Character.screenAdjustX)
         setScreenPositionY(float(Character.controlled.y) - Character.screenAdjustY)
@@ -211,6 +212,11 @@ def centerScreen():
                 Character.canvas.coords(PlatformerTextbox.textBox.textShown['arrow'],
                                         730 + adjustX,
                                         610 + adjustY)
+        netChange = Character.canvas.xview()[0] - previous
+        for P in background.instances:
+            if P.layer:
+                P.x -= netChange * 0.1 * P.layer * Character.xLevelSize
+                Character.canvas.coords(P.imageID, float(P.x), float(P.y))
 
 
 def SweptAABB(x1, y1, x1_, y1_, width1, height1, x2, y2, x2_, y2_, width2, height2, dt):
@@ -709,61 +715,6 @@ class background:
         self.imageID = Character.canvas.create_image(x, y, anchor='nw', image=self.image)
 
 
-def startGame():
-    text = Character.canvas.create_text(width / 2, height / 2, text="Press X to start", fill="white", font="system 18")
-    textFlicker(Character.root, Character.canvas, text)
-    Character.canvas.bind("<KeyPress-z>", lambda event: startOpeningScene())
-
-
-def textFlicker(root, canvas, text):
-    """Switches the color of text from white to black every 0.5 seconds"""
-    if canvas.itemcget(text, 'fill') == 'white':
-        canvas.itemconfigure(text, fill='black')
-    else:
-        canvas.itemconfigure(text, fill='white')
-    loop = root.after(500, textFlicker, root, canvas, text)
-    Character.loopsRunning.append(loop)
-
-
-def startOpeningScene():
-    clearWindow()
-    Character.canvas.create_text(width / 2, height / 2, text=" by Jack Meyer Garvey", fill="white",
-                                 font="system 32")
-    loop = Character.root.after(2500, openingScene)
-    Character.loopsRunning.append(loop)
-
-
-def openingScene():
-    clearWindow()
-    background('Clouds1.png', 1).spawn(0, 0)
-    background('Clouds2.png', 2).spawn(0, 0)
-    background('Clouds3.png', 3).spawn(0, 0)
-    Character('WhiteFloor.png', dynamic=False).spawnChar(300, 900+300)
-    Character('WhiteFloor.png', dynamic=False).spawnChar(760, 500+300)
-    You = Character('BlueBoxDot.png')
-    You.y__['gravity'] = Fraction(0.004)
-    You.spawnChar(760, 200+300)
-    You = Character('BlueBoxDot.png')
-    You.y__['gravity'] = Fraction(0.004)
-    You.spawnChar(760, 300)
-    You = Character('BlueBoxDot.png')
-    You.y__['gravity'] = Fraction(0.004)
-    You.spawnChar(900, 1060)
-    Character('WhiteWall.png', dynamic=False).spawnChar(700, 780+300)
-    Character('WhiteWall.png', dynamic=False).spawnChar(2050, 300+300)
-    PlatformerTextbox.runQueue()
-
-    sign = funcHold('Sign.png')
-    sign.queueText("Do you like it?", options=["Yes", "No", " Maybe LOL"])
-    sign.spawn(400, 760+300)
-
-    You = Character(pic='BlueBox.png', pic2='WhiteBox.png')
-    You.spawnChar(300, 900 - 70 + 300)
-    You.gainControl()
-    setLevelSize(2400, 1500)
-    physicsLoop()
-
-
 def clearWindow():
     """Destroys all widgets and ends all loops (including loops within text boxes)"""
     Character.instances.clear()
@@ -791,7 +742,7 @@ def clearWindow():
     Character.oncePerFrame = []
     Character.grid = 300
     Character.dt = 20
-    Character.canvas = Canvas(Character.root, width=width, height=height, bg='black')
+    Character.canvas = Canvas(Character.root, width=1330, height=845, bg='black')
     PlatformerTextbox.textBox.canvas = Character.canvas
     Character.canvas.focus_set()
     Character.canvas.pack()
@@ -804,22 +755,3 @@ def clearWindow():
 
     funcHold.instances = []
     funcHold.hashTable = {}
-
-
-width = 1330
-height = 845
-Character.root = Tk()
-PlatformerTextbox.textBox.root = Character.root
-Character.root.geometry(f"{width}x{height}")
-Character.root.configure(background='black')
-Character.root.attributes("-fullscreen", True)
-Character.root.title("PlatFormer")
-Character.canvas = Canvas(Character.root, width=width, height=height, bg='black')
-PlatformerTextbox.textBox.canvas = Character.canvas
-Character.canvas.focus_set()
-
-startGame()
-Character.start = time()
-
-Character.canvas.pack()
-mainloop()
---
+++ main.py
@@ -0,0 +1,75 @@
+from continuousCollision import *
+
+
+def startGame():
+    text = Character.canvas.create_text(width / 2, height / 2, text="Press X to start", fill="white", font="system 18")
+    textFlicker(Character.root, Character.canvas, text)
+    Character.canvas.bind("<KeyPress-z>", lambda event: startOpeningScene())
+
+
+def textFlicker(root, canvas, text):
+    """Switches the color of text from white to black every 0.5 seconds"""
+    if canvas.itemcget(text, 'fill') == 'white':
+        canvas.itemconfigure(text, fill='black')
+    else:
+        canvas.itemconfigure(text, fill='white')
+    loop = root.after(500, textFlicker, root, canvas, text)
+    Character.loopsRunning.append(loop)
+
+
+def startOpeningScene():
+    clearWindow()
+    Character.canvas.create_text(width / 2, height / 2, text=" by Jack Meyer Garvey", fill="white",
+                                 font="system 32")
+    loop = Character.root.after(2500, openingScene)
+    Character.loopsRunning.append(loop)
+
+
+def openingScene():
+    clearWindow()
+    background('Clouds1.png', 0).spawn(0, 0)
+    background('Clouds2.png', 2).spawn(0, 0)
+    background('Clouds3.png', 1).spawn(0, 0)
+    Character('WhiteFloor.png', dynamic=False).spawnChar(300, 900 + 300)
+    Character('WhiteFloor.png', dynamic=False).spawnChar(760, 500 + 300)
+    You = Character('BlueBoxDot.png')
+    You.y__['gravity'] = Fraction(0.004)
+    You.spawnChar(760, 200 + 300)
+    You = Character('BlueBoxDot.png')
+    You.y__['gravity'] = Fraction(0.004)
+    You.spawnChar(760, 300)
+    You = Character('BlueBoxDot.png')
+    You.y__['gravity'] = Fraction(0.004)
+    You.spawnChar(900, 1060)
+    Character('WhiteWall.png', dynamic=False).spawnChar(700, 780 + 300)
+    Character('WhiteWall.png', dynamic=False).spawnChar(2050, 300 + 300)
+    PlatformerTextbox.runQueue()
+
+    sign = funcHold('Sign.png')
+    sign.queueText("Do you like it?", options=["Yes", "No", " Maybe LOL"])
+    sign.spawn(400, 760 + 300)
+
+    You = Character(pic='BlueBox.png', pic2='WhiteBox.png')
+    You.spawnChar(300, 900 - 70 + 300)
+    You.gainControl()
+    setLevelSize(2400, 1500)
+    physicsLoop()
+
+
+width = 1330
+height = 845
+Character.root = Tk()
+PlatformerTextbox.textBox.root = Character.root
+Character.root.geometry(f"{width}x{height}")
+Character.root.configure(background='black')
+Character.root.attributes("-fullscreen", True)
+Character.root.title("PlatFormer")
+Character.canvas = Canvas(Character.root, width=width, height=height, bg='black')
+PlatformerTextbox.textBox.canvas = Character.canvas
+Character.canvas.focus_set()
+
+startGame()
+Character.start = time()
+
+Character.canvas.pack()
+mainloop()
